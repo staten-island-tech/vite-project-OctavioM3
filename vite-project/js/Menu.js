1ba01d7f617b5aa3f9e98d6951a22ebb339c478f
OctavioM3 <octaviom3@nycstudents.net>
2023-11-14 09:21:52 -0500

    sresttrgdrg

--- vite-project/js/Menu.js
+++ vite-project/js/Menu.js
@@ -1,34 +1,53 @@
 const Menu = [
-    {
-        name: "Vegetarian Pasta",
-        price: 12.99,
-        vegetarian: true,
-        inStock: true
-      },
-      {
-        name: "Chicken Burger",
-        price: 9.99,
-        vegetarian: false,
-        inStock: true
-      },
-      {
-        name: "Margherita Pizza",
-        price: 15.99,
-        vegetarian: true,
-        inStock: false
-      },
-      {
-        name: "Grilled Salmon",
-        price: 18.99,
-        vegetarian: false,
-        inStock: true
-      },
-      {
-        name: "Fries",
-        price: 5.00,
-        vegetarian: true,
-        inStock: true,
-      },
+{
+    name: "Pizza Pie",
+    vegetarian: false,
+    price: 13.00,
+    inStock: true,
+},
+{
+    name: "Beef Burger",
+    vegetarian: false,
+    price: 10.00,
+    inStock: true,
+},
+{
+    name: "French Fries",
+    vegetarian: true,
+    price: 6.00,
+    inStock: true,
+},
+{
+    name: "Ceasar Salad",
+    vegetarian: true,
+    price: 10.00,
+    inStock: false,
+},
+{
+    name: "Spaghetti Meatballs",
+    vegetarian: false,
+    price: 11.00,
+    inStock: false,
+},
+{
+    name: "Potato Salad",
+    vegetarian: true,
+    price: 12.00,
+    inStock: true,
+},
+{
+    name: "Grilled Salmon",
+    vegetarian: false,
+    price: 10.00,
+    inStock: false,
+},
+{
+    name: "Vegetarian Pasta",
+    vegetarian: true,
+    price: 15.00,
+    inStock: true,
+},
 ]
+
 
 export { Menu };
