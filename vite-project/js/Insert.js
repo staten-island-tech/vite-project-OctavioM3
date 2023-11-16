c375d09298e7d4b5abe16284796be08bf8b19a5b
OctavioM3 <octaviom3@nycstudents.net>
2023-11-16 09:24:43 -0500

    erwggerwgewr

--- vite-project/js/Insert.js
+++ vite-project/js/Insert.js
@@ -1,11 +1,23 @@
 import { Menu } from "./Menu";
 import { DOMSelectors } from "./Dom";
 
-function insertfullMenu () {
-Menu.forEach((Meal) => DOMSelectors.CardBox.insertAdjacentHTML (
-    "beforeend",
-    `<div class="MealCard"><h3> ${Meal.name} </h3><img src="${Meal.img}" alt="${Meal.name}" class="Cardimg"></div>`
-));
-};
+function insertfullMenu() {
+  Menu.forEach((Meal) =>
+    DOMSelectors.CardBox.insertAdjacentHTML(
+      "beforeend",
+      `<div class="MealCard"><h3> ${Meal.name} </h3><img src="${Meal.img}" alt="${Meal.name}" class="Cardimg"></div>`
+    )
+  );
+}
+
+function clearAll() {
+  let remove = document.querySelectorAll(".menubtn");
+  remove.forEach((erase) => {
+    erase.addEventListener("click", function (secondevent) {
+      secondevent.target.parentElement.remove();
+    });
+  });
+}
 
 export { insertfullMenu };
+export { clearAll };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,10 +2,12 @@ import "../css/style.css";
 import { Menu } from "./Menu";
 import { DOMSelectors } from "./Dom";
 import { insertfullMenu } from "./Insert";
+import { clearAll } from "./Insert";
 
 console.log(Menu);
 
 DOMSelectors.SeeFullMenuBtn.addEventListener("click", function (event) {
     event.preventDefault();
     insertfullMenu();
+    clearAll();
 });
