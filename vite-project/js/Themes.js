eb8f8b44ea0904611a45285ad8972c42954142a6
OctavioM3 <octaviom3@nycstudents.net>
2023-11-26 01:51:06 -0500

    dfsdfdsfs

--- vite-project/js/Themes.js
+++ vite-project/js/Themes.js
@@ -3,13 +3,16 @@ import { DOMSelectors } from "./Dom";
 function Themes() {
 if (DOMSelectors.ThemeSelector.value === "light") {
     document.body.classList.add("Light");
-    document.body.classList.remove("Dark");
+    document.body.classList.remove("Dark", "Green");
   } else if (DOMSelectors.ThemeSelector.value === "dark") {
     document.body.classList.add("Dark");
-    document.body.classList.remove("Light");
+    document.body.classList.remove("Light", "Green");
+  } else if (DOMSelectors.ThemeSelector.value === "green") {
+    document.body.classList.add("Green");
+    document.body.classList.remove("Dark", "Light");
   } else {
     document.body.classList.remove("Dark");
-    document.body.classList.remove("Light");
+    document.body.classList.remove("Light", "Green");
   }
 }
 
