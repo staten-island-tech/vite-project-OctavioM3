8608e40309ded52ae235909fb0f9b9256ce8ba16
OctavioM3 <octaviom3@nycstudents.net>
2023-11-29 09:22:34 -0500

    dfsgsdfgdfg

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,12 +2,7 @@ import "../css/style.css";
 import { DOMSelectors } from "./Dom";
 import { insertfullMenu, clearAll, VegetarianMenu, CheapMenu, InStockMenu } from "./Insert";
 import { Themes } from "./Themes";
-
-DOMSelectors.MenuButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  clearAll();
-  insertfullMenu();
-});
+import { Menu } from "./Menu";
 
 DOMSelectors.VegetarianButton.addEventListener("click", function (event) {
   event.preventDefault();
@@ -31,3 +26,16 @@ DOMSelectors.ThemeSelector.addEventListener("change", function (event) {
   event.preventDefault();
   Themes();
 });
+
+let buttons = document.querySelectorAll(".btns")
+buttons.forEach((btn) => btn.addEventListener("click", function(SeeMenu){
+  clearAll(SeeMenu)
+  let filter = btn.textContent
+  if (btn == buttons[0]) {
+    insertfullMenu()
+  } else if (btn == buttons[4]) {
+    InStockMenu()
+  }
+}));
+
+Menu.filter((SeeMenu) => SeeMenu.includes(filter)).forEach((SeeMenu)=>insertfullMenu(SeeMenu));
